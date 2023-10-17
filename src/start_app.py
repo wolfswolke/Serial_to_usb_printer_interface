916c5d13a21ca1cd1c000fc69d21d737c8ac9018
ZKWolf <zkwolf@zkwolf.com>
2023-10-17 15:39:16 +0200

    Added THREADING and now using Logic.

--- src/start_app.py
+++ src/start_app.py
@@ -1,31 +1,43 @@
+import time
+from threading import Thread
+
 from logic.logging_handler import logger
+from logic.serial_handler import serial_handler
 from logic.print_handler import print_handler
-import serial
+
+
+def keep_alive():
+    try:
+        t = Thread(target=run)
+        t.daemon = True
+        t.start()
+        while True:
+            time.sleep(100)
+    except (KeyboardInterrupt, SystemExit):
+        logger.log(level="info", handler="serial_handler", message={"event": "Service Stopped"})
+        serial_handler.close()
+        exit(0)
+
+
+def run():
+    data = ""
+    while True:
+        line = serial_handler.receive()
+        if line:
+            logger.log(level="debug", handler="serial_handler", message={"event": "Serial Data", "data": line})
+        try:
+            data += line.decode("utf-8")
+        except UnicodeDecodeError:
+            data += line
+        if "^XZ" in data:
+            logger.log(level="info", handler="serial_handler", message={"event": "Label Received"})
+            logger.log(level="debug", handler="serial_handler", message={"event": "Label Data", "data": data})
+            print_handler.print(data)
+            data = ""
+
 
 if __name__ == "__main__":
     logger.setup_logger()
-    # serial_handler.setup(baud=115200, timeout=3.0, interface="COM8")
-    ser = serial.Serial("COM8", baudrate=115200) # /dev/ttyUSB0
-    print_handler.setup("ZTC-ZD421-203dpi-ZPL")
-
-    try:
-        data = ""
-        while True:
-            line = ser.readline()
-            if line:
-                logger.log(level="debug", handler="serial_handler", message={"event": "Serial Data", "data": line})
-            try:
-                data += line.decode("utf-8")
-            except UnicodeDecodeError:
-                data += line
-            if "^XZ" in data:
-                logger.log(level="info", handler="serial_handler", message={"event": "Label Received"})
-                logger.log(level="debug", handler="serial_handler", message={"event": "Label Data", "data": data})
-                print_handler.print(data)
-                data = ""
-        ser.close()
-
-    except KeyboardInterrupt:
-        ser.close()
-        logger.log(level="info", handler="serial_handler", message={"event": "Service Stopped"})
-        exit(0)
+    serial_handler.setup(baud=115200, interface="COM8")  # /dev/ttyUSB0
+    print_handler.setup("ZDesigner ZD421-203dpi ZPL") # ZTC-ZD421-203dpi-ZPL
+    keep_alive()
